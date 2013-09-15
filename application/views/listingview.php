6e73dedbe21e84b557cf7d84a6db2002a6e6684e
Christopher Noland <bangb12@fullsail.edu>
2013-09-15 12:57:38 -0400

    basic view for listings page complete

---
+++ application/views/listingview.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Images - ImageBase</title>
+    <link href="css/bootstrap/css/bootstrap.css" rel="stylesheet"></script>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1 class=".text-center">Image Base</h1>
+            <div class="nav-header">
+                <a href="#">Home</a>
+                <a href="#">Log-in</a>
+            </div>
+        </header>
+        <section>
+            <div class="container-fluid">
+                <h2>Image Name</h2>
+                <img src="images/placeholder.jpg"/>
+                <p>Short description of image</p>
+            </div>
+        </section>
+        <section>
+            <div class="container-fluid">
+                <h2>Image Name</h2>
+                <img src="images/placeholder.jpg"/>
+                <p>Short description of image</p>
+            </div>
+        </section>
+        <section>
+            <div class="container-fluid">
+                <h2>Image Name</h2>
+                <img src="images/placeholder.jpg"/>
+                <p>Short description of image</p>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
