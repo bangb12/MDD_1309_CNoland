e85fc776c10eb0844d86e01644b76935cbba2c08
Christopher Noland <bangb12@fullsail.edu>
2013-09-20 14:21:49 -0400

    corrected header comment

--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,7 @@
     //Header of the website
     <header class="nav-header">
             <h1 class=".text-center">Image Base</h1>
-            //Navigation including links
+            <!---- Navigation including links ---->
             <div class="nav">
                 <a href="<? echo base_url() ?>/welcome/index/">Home</a>
                 <a href="index.php/login/view/">Log-in</a>
